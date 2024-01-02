b00f8ed2d44bf4c8d487e50a7369070fd86991a8
lkbrian <lkbrian742@gmail.com>
2024-01-02 11:49:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+function submitData(name, email) {
+    return fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const userID = document.querySelector(".id");
+          userID.textContent = `user id :${data.id} `;
+        document.body.append(userID);
+      })
+      .catch((error) => {
+        let errors = document.querySelector(".error");        
+        errors.textContent = error.message
+        document.body.append(errors);
+      });
+  }
+  
